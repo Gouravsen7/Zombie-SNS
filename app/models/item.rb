e8b57503d6065a045620b00f0aacf100ebc06a1f
Gouravsen7 <gourav031192@gmail.com>
2024-04-08 20:47:23 +0530

    add validation for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
 	belongs_to :survivor
+
+	def is_item_valid?(str)
+		item.include?(str)
+	end
 end
--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,10 +1,21 @@
 class Survivor < ApplicationRecord
 	has_many :items
+	accepts_nested_attributes_for :items
 
 	enum gender: [:female, :male, :other]
-
-	validates :name, presence: true
+	validates :name, uniqueness: true
 	validates :age, presence: true, numericality: true
-	validates :latitude , numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }
-	validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+	validates :latitude , numericality: { format: { with: /-?\d{1,6}\.\d{1,3}/}}
+	validates :longitude, numericality: { format: { with: /-?\d{1,6}\.\d{1,3}/}}
+	validate :check_items
+
+	def check_items
+	  items.each do |inventory|
+	    water_exist = inventory.is_item_valid?('water')
+	    aid_exist = inventory.is_item_valid?('first aid')
+	    unless water_exist && aid_exist
+	  		errors.add(:item, 'water and first aid must exist') 
+	    end
+	  end
+	end
 end
